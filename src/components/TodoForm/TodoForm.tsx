b93a2fb863a908fc290b4e901d2bb9d6b2dcf933
Kim-Jaemin420 <jany240@naver.com>
2022-08-19 03:16:39 +0900

    Feat: uncontrolled 방식으로 todo input 관리

--- src/components/TodoForm/TodoForm.tsx
+++ src/components/TodoForm/TodoForm.tsx
@@ -4,12 +4,28 @@ import styles from './todoForm.module.scss';
 const { formWrapper } = styles;
 
 function TodoForm() {
+  const handleSubmitForm = (event: React.FormEvent) => {
+    event.preventDefault();
+  };
+
+  const handleChangeForm = (event: React.ChangeEvent<HTMLFormElement>) => {
+    let isSubmitDisabed = false;
+    const { todoField, submitField } = event.currentTarget;
+    console.log(todoField, submitField);
+
+    if (!todoField.value) {
+      isSubmitDisabed = true;
+    }
+
+    submitField.disabled = isSubmitDisabed;
+  };
+
   return (
-    <form action="" className={formWrapper}>
+    <form className={formWrapper} onSubmit={handleSubmitForm} onChange={handleChangeForm}>
       <fieldset>
         <legend className="a11yHidden">Add Todo</legend>
-        <input type="text" placeholder="today's todo" />
-        <input type="submit" value="추가" />
+        <input type="text" name="todoField" placeholder="today's todo" />
+        <input type="submit" name="submitField" value="ADD" disabled />
       </fieldset>
     </form>
   );
