2eb69a48089a7a42289446b76e084fba4029c588
Kim-Jaemin420 <jany240@naver.com>
2022-08-19 02:41:58 +0900

    Feat: uncontrolled 방식으로 로그인 input 관리

--- src/pages/Signin/Signin.tsx
+++ src/pages/Signin/Signin.tsx
@@ -4,16 +4,53 @@ import styles from './signin.module.scss';
 const { wrapper } = styles;
 
 function Signin() {
+  const validateEmailCondition = (email: string) => {
+    const EMAIL_REGEX = /^[0-9a-zA-Z]([-_.]?[0-9a-zA-Z])*@[0-9a-zA-Z]([-_.]?[0-9a-zA-Z])*.[a-zA-Z]{2,3}$/i;
+
+    if (EMAIL_REGEX.test(email)) {
+      return true;
+    }
+    return false;
+  };
+
+  const validatePasswordCondition = (password: string) => {
+    const PASSWORD_MINIMUM_LENGTH = 8;
+
+    if (password.length >= PASSWORD_MINIMUM_LENGTH) {
+      return true;
+    }
+    return false;
+  };
+
+  const handleSubmitForm = (event: React.FormEvent) => {
+    console.log(event);
+
+    event.preventDefault();
+  };
+
+  const handleChangeForm = (event: React.ChangeEvent<HTMLFormElement>) => {
+    let isSubmitDisabled = false;
+    const { emailField, passwordField, submitField } = event.currentTarget;
+    const email = emailField.value;
+    const password = passwordField.value;
+
+    if (!validateEmailCondition(email) || !validatePasswordCondition(password)) {
+      isSubmitDisabled = true;
+    }
+
+    submitField.disabled = isSubmitDisabled;
+  };
+
   return (
     <div className={wrapper}>
-      <form action="">
+      <form onSubmit={handleSubmitForm} onChange={handleChangeForm}>
         <fieldset>
           <legend>Sign in</legend>
           <label htmlFor="email">ID</label>
-          <input type="email" name="email" id="" placeholder="email" />
+          <input type="email" name="emailField" id="" placeholder="email" />
           <label htmlFor="password">Password</label>
-          <input type="password" name="password" id="" placeholder="password" />
-          <input type="submit" value="sign in" />
+          <input type="password" name="passwordField" id="" placeholder="password" />
+          <input type="submit" name="submitField" value="sign in" disabled />
         </fieldset>
       </form>
     </div>
