150b816f7f5561340d0641a7907a2a397b929498
Kim-Jaemin420 <jany240@naver.com>
2022-08-19 01:09:44 +0900

    Feat: sign in page style

--- src/pages/Signin/Signin.tsx
+++ src/pages/Signin/Signin.tsx
@@ -1,7 +1,23 @@
 import React from 'react';
+import styles from './signin.module.scss';
+
+const { wrapper } = styles;
 
 function Signin() {
-  return <div>Signin</div>;
+  return (
+    <div className={wrapper}>
+      <form action="">
+        <fieldset>
+          <legend>Sign in</legend>
+          <label htmlFor="email">ID</label>
+          <input type="email" name="email" id="" placeholder="email" />
+          <label htmlFor="password">Password</label>
+          <input type="password" name="password" id="" placeholder="password" />
+          <input type="submit" value="sign in" />
+        </fieldset>
+      </form>
+    </div>
+  );
 }
 
 export default Signin;
