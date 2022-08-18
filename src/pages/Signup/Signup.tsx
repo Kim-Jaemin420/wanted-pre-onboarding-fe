cee369032a99f78b2a3d6f1361eb61650c4a9a12
Kim-Jaemin420 <jany240@naver.com>
2022-08-19 03:31:56 +0900

    Feat: uncontrolled 방식으로 signup input 관리

--- src/pages/Signup/Signup.tsx
+++ src/pages/Signup/Signup.tsx
@@ -1,7 +1,49 @@
 import React from 'react';
+import { validateEmailCondition, validatePasswordCondition } from '../../utils';
+import styles from './signup.module.scss';
+
+const { wrapper } = styles;
 
 function Signup() {
-  return <div>signup</div>;
+  const handleSubmitForm = (event: React.FormEvent) => {
+    event.preventDefault();
+  };
+
+  const handleChangeForm = (event: React.ChangeEvent<HTMLFormElement>) => {
+    let isSubmitDisabled = false;
+    const { emailField, passwordField, passwordConfirmField, submitField } = event.currentTarget;
+    const email = emailField.value;
+    const password = passwordField.value;
+    const passwordConfirm = passwordConfirmField.value;
+
+    if (
+      !validateEmailCondition(email) ||
+      !validatePasswordCondition(password) ||
+      !validatePasswordCondition(passwordConfirm) ||
+      password !== passwordConfirm
+    ) {
+      isSubmitDisabled = true;
+    }
+
+    submitField.disabled = isSubmitDisabled;
+  };
+
+  return (
+    <div className={wrapper}>
+      <form onSubmit={handleSubmitForm} onChange={handleChangeForm}>
+        <fieldset>
+          <legend>Sign up</legend>
+          <label htmlFor="email">ID</label>
+          <input type="email" name="emailField" id="" placeholder="email" />
+          <label htmlFor="password">Password</label>
+          <input type="password" name="passwordField" id="" placeholder="password" />
+          <label htmlFor="passwordConfirm">Password Confirm</label>
+          <input type="password" name="passwordConfirmField" id="" placeholder="password confirm" />
+          <input type="submit" name="submitField" value="sign up" disabled />
+        </fieldset>
+      </form>
+    </div>
+  );
 }
 
 export default Signup;
