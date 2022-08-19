241076f63c27ba9528f4bfb7662466d0696c1806
Kim-Jaemin420 <jany240@naver.com>
2022-08-19 14:45:57 +0900

    Feat: todo list 컴포넌트 구조 설정

--- src/components/TodoItem/TodoItem.tsx
+++ src/components/TodoItem/TodoItem.tsx
@@ -1,13 +1,26 @@
 import React from 'react';
 import styles from './todoItem.module.scss';
 
-const { todoItem } = styles;
+const { todoItem, todoContent } = styles;
 
 function TodoItem() {
+  const handleChangeCheckbox = (event: React.ChangeEvent<HTMLInputElement>) => {
+    console.log(event.target);
+  };
+
   return (
     <li className={todoItem}>
-      <input type="checkbox" name="" id="" />
-      javacript
+      <input
+        type="checkbox"
+        name="complete"
+        id="complete"
+        className="a11yHidden"
+        checked={true}
+        onChange={handleChangeCheckbox}
+      />
+      <label htmlFor="complete">
+        <span className={todoContent}>javacriptjavacripjavacriptjavacrip</span>
+      </label>
       <button>delete</button>
     </li>
   );
