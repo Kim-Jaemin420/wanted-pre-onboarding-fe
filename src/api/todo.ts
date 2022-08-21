28c6dd7bc87c74e203835dc80a93e6b3b022d771
Kim-Jaemin420 <jany240@naver.com>
2022-08-21 12:42:32 +0900

    Modify: parameter로 받는 타입 변경

--- src/api/todo.ts
+++ src/api/todo.ts
@@ -15,14 +15,14 @@ export const createTodo = ({ todo }: Partial<Todo>) => {
   });
 };
 
-export const updateTodo = ({ id, todo, isCompleted }: Todo) => {
+export const updateTodo = ({ id, todo, isCompleted }: Partial<Todo>) => {
   return http.put({
     url: `${TODOS_PAGE}/${id}`,
     data: { todo, isCompleted },
   });
 };
 
-export const deleteTodo = ({ id }: Todo) => {
+export const deleteTodo = ({ id }: Partial<Todo>) => {
   return http.delete({
     url: `${TODOS_PAGE}/${id}`,
   });
