1c03e1af000c7db0370feeb593d4cff5f8044c92
Kim-Jaemin420 <jany240@naver.com>
2022-08-23 03:30:32 +0900

    Feat: todos 라우트 상수 추가

--- src/consts/route.ts
+++ src/consts/route.ts
@@ -2,3 +2,4 @@ export const HOME_PAGE = '/';
 export const SIGNIN_PAGE = '/auth/signin';
 export const SIGNUP_PAGE = '/auth/signup';
 export const TODO_PAGE = '/todo';
+export const TODOS_PAGE = '/todos';
