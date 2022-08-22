a249f370f50d6b5073a9f1da864b1105674af661
Kim-Jaemin420 <jany240@naver.com>
2022-08-23 02:30:32 +0900

    Feat: db에 저장된 todo를 렌더링

--- src/pages/Todolist/Todolist.tsx
+++ src/pages/Todolist/Todolist.tsx
@@ -1,16 +1,20 @@
-import React, { useEffect } from 'react';
+import React, { useEffect, useState } from 'react';
 import { getTodos } from '../../api';
 import { TodoForm, TodoItem } from '../../components';
+import { Todo } from '../../types';
 import styles from './todolist.module.scss';
 
 const { wrapper, todoList } = styles;
 
 function Todolist() {
+  const [todos, setTodos] = useState<Todo[]>([]);
+
   useEffect(() => {
     const fetchData = async () => {
       try {
-        const result = await getTodos();
-        console.log(result);
+        const { data } = await getTodos();
+        setTodos(data);
+        console.log(data);
       } catch (error) {
         console.log(error);
       }
@@ -23,19 +27,11 @@ function Todolist() {
     <div className={wrapper}>
       <div>
         <h1>todo</h1>
-        <TodoForm />
+        <TodoForm setTodos={setTodos} />
         <ul className={todoList}>
-          <TodoItem />
-          <TodoItem />
-          <TodoItem />
-          <TodoItem />
-          <TodoItem />
-          <TodoItem />
-          <TodoItem />
-          <TodoItem />
-          <TodoItem />
-          <TodoItem />
-          <TodoItem />
+          {todos.map(({ todo, id, isCompleted }) => (
+            <TodoItem key={id} id={id} todoContent={todo} isCompleted={isCompleted} todos={todos} setTodos={setTodos} />
+          ))}
         </ul>
       </div>
     </div>
